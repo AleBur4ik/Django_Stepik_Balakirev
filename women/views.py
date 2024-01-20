6cc90d1773450f15f182045f05df7be17271c709
Alex-PC-home <bur4ik85@mail.ru>
2024-01-21 00:13:37 +0300

    2.4 lesson, edit files

--- women/views.py
+++ women/views.py
@@ -2,13 +2,27 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 from django.template.loader import render_to_string
 
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли',
+     'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс',
+     'is_published': True},
+]
+
 
 def index(request):
-    return render(request, 'women/index.html')
+    data = {'title': 'Главная страница',
+            'menu': menu,
+            'posts': data_db}
+    return render(request, 'women/index.html', context=data)
 
 
 def about(request):
-    return render(request, 'women/about.html')
+    data = {'title': 'О сайте'}
+    return render(request, 'women/about.html', context=data)
 
 
 def categories(request, cat_id):
