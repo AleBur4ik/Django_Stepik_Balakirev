171d5419aa701503a69bbbea67ec51c173ec497d
Alex-PC-home <bur4ik85@mail.ru>
2024-01-23 23:28:03 +0300

    2.8 lesson, edit/create file

--- women/views.py
+++ women/views.py
@@ -18,15 +18,23 @@ data_db = [
 
     {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
 
-    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс',
+     'is_published': True},
 
+]
+
+cats_db = [
+    {'id': 1, 'name': 'Актрисы'},
+    {'id': 2, 'name': 'Певицы'},
+    {'id': 3, 'name': 'Спортсменки'},
 ]
 
 
 def index(request):
     data = {'title': 'Главная страница',
             'menu': menu,
-            'posts': data_db}
+            'posts': data_db,
+            'cat_selected': 0,}
     return render(request, 'women/index.html', context=data)
 
 
@@ -53,3 +61,11 @@ def contact(request):
 
 def login(request):
     return HttpResponse('Авторизация')
+
+
+def show_category(request, cat_id):
+    data = {'title': 'Отображение по рубрикам',
+            'menu': menu,
+            'posts': data_db,
+            'cat_selected': cat_id, }
+    return render(request, 'women/index.html', context=data)
