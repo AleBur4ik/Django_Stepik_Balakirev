51c80b1ce7f198b9a9ff6912d2e9740e48750c91
Alexey-Vivobook <bur4ik85@mail.ru>
2024-02-04 01:13:06 +0300

    4.6 lesson

--- women/views.py
+++ women/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
 
-from .models import Women
+from .models import Women, Category, TagPost
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -25,19 +25,13 @@ data_db = [
 
 ]
 
-cats_db = [
-    {'id': 1, 'name': 'Актрисы'},
-    {'id': 2, 'name': 'Певицы'},
-    {'id': 3, 'name': 'Спортсменки'},
-]
-
 
 def index(request):
     posts = Women.published.all()
     data = {'title': 'Главная страница',
             'menu': menu,
             'posts': posts,
-            'cat_selected': 0,}
+            'cat_selected': 0, }
     return render(request, 'women/index.html', context=data)
 
 
@@ -75,9 +69,26 @@ def login(request):
     return HttpResponse('Авторизация')
 
 
-def show_category(request, cat_id):
-    data = {'title': 'Отображение по рубрикам',
+def show_category(request, cat_slug):
+    category = get_object_or_404(Category, slug=cat_slug)
+    posts = Women.published.filter(cat_id=category.pk)
+
+    data = {'title': f'Рубрика: {category.name}',
             'menu': menu,
-            'posts': data_db,
-            'cat_selected': cat_id, }
+            'posts': posts,
+            'cat_selected': category.pk, }
+    return render(request, 'women/index.html', context=data)
+
+
+def show_tag_postlist(request, tag_slug):
+    tag = get_object_or_404(TagPost, slug=tag_slug)
+    posts = tag.tags.filter(is_published=Women.Status.PUBLISHED)
+
+    data = {
+        'title': f"Тэг: {tag.tag}",
+        'menu': menu,
+        'posts': posts,
+        'cat_selected': None,
+    }
+
     return render(request, 'women/index.html', context=data)
