9026583a783adf8257d7affdb32aa9192938027c
Alex-PC-home <bur4ik85@mail.ru>
2024-02-21 23:06:03 +0300

    7.4 lesson

--- women/models.py
+++ women/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.db import models
 from django.urls import reverse
 from transliterate import slugify
@@ -14,12 +15,19 @@ class Women(models.Model):
         PUBLISHED = 1, 'Опубликовано'
 
     title = models.CharField(max_length=255, verbose_name='Заголовок')
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug',
+                            validators=[
+                                MinLengthValidator(5, message='мало символов!!! минимум 5!'),
+                                MaxLengthValidator(100, message='че так много!? максимум 100')
+                            ])
     content = models.TextField(blank=True, verbose_name='Текст статьи')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
-    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)), default=Status.DRAFT, verbose_name='Статус')
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts', verbose_name='Категории')
+    is_published = models.BooleanField(
+        choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)), default=Status.DRAFT,
+        verbose_name='Статус')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts',
+                            verbose_name='Категории')
     tags = models.ManyToManyField('TagPost', blank=True, related_name='tags', verbose_name='Теги')
     husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True,
                                    related_name='wuman', verbose_name='Муж')
