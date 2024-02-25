384a7bbe34579e8f071ecc9f94bc02961aa4eb71
Alex-PC-home <bur4ik85@mail.ru>
2024-02-25 16:48:16 +0300

    7.8 lesson

--- women/models.py
+++ women/models.py
@@ -20,6 +20,7 @@ class Women(models.Model):
                                 MinLengthValidator(5, message='мало символов!!! минимум 5!'),
                                 MaxLengthValidator(100, message='че так много!? максимум 100')
                             ])
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', default=None, blank=True, null=True, verbose_name='Фото')
     content = models.TextField(blank=True, verbose_name='Текст статьи')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
@@ -88,3 +89,7 @@ class Husband(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UploadFiles(models.Model):
+    file = models.FileField(upload_to='uploads_model')
